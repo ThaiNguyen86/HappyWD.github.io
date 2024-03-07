877bc385ffe3011457d3df7be197f061ba2833b8
ThaiNguyen86 <ngtthai0604@gmail.com>
2024-03-07 23:19:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showGreeting() {
             "Chúc " + name + " ngày càng xinh đẹp, thành công nha 🌠",
             "Chúc " + name + " kì này lụm học bổng 😆",
             "Chúc " + name + " có một ngày 8/3 thật vui tươi, tràn ngập hạnh phúc nhé 😁 ",
-            "Chúc " + name + " nhận được nhiều quà, nhiều hoa, nhiều lời chúc của phái nam trong ngày đặc biệt hôm nay nha 🌹 "
+            "Chúc " + name + " nhận được nhiều quà, nhiều hoa nhiều lời chúc của phái nam \n trong ngày đặc biệt hôm nay nha 🌹 "
         ];
         var randomIndex = Math.floor(Math.random() * randomGreetings.length);
         var greeting = randomGreetings[randomIndex];
